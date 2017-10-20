de4a119e63422546604a51823c59c1037b3ee59b
alexandre033 <beaudouina@gmail.com>
2017-10-20 17:08:13 +0200

    add mangoose

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 require('./services/passport');
-const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+//DB connect
+mongoose.connect(keys.mongoURI);
+
+const app = express();
 //Call the arrow function in routes and pass the argument app
 require('./routes/authRoutes')(app);
 
